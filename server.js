3a2aa652eee0bb05fedf5a04bddfa69b1a8fcdd5
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-05-13 16:57:02 -0400

    updated port number

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.static(__dirname + '/public'));
 require ("./test/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 80;
 
 app.listen(port, ipaddress);
