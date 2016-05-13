ff66da42140ce252e7e5c3acabb0da8e8ac59906
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-05-13 19:01:31 -0400

    updated

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
+/**
+ * Created by zhangyuxi on 2016/5/13.
+ */
 var express = require('express');
 var app = express();
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.listen(port, ipaddress);
+
+app.listen(port,ipaddress);
+
