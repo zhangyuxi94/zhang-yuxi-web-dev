ba8b21356fc85732c1e30f444689f13a4cff1ee0
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-05-13 16:50:22 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.listen(port, ipaddress);
