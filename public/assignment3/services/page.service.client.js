910f0a4ddb60c950bd289a5ccffbae60d780c81a
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-05-28 00:37:47 -0400

    added angularjs in page-list

--- public/assignment3/services/page.service.client.js
+++ public/assignment3/services/page.service.client.js
@@ -5,23 +5,32 @@
     angular.module("WebAppMaker")
         .factory("PageService",PageService);
     function PageService(){
-        var page=[
+        var pages=[
             { "_id": "321", "name": "Post 1", "websiteId": "456" },
             { "_id": "432", "name": "Post 2", "websiteId": "456" },
             { "_id": "543", "name": "Post 3", "websiteId": "456" }
         ];
         var api={
-            "createPage":"createPage",
-            "findPageByWebsiteId":"findPageByWebsiteId",
-            "findPageById":"findPageById",
-            "updatePage":"updatePage",
-            "deletePage":"deletePage"
+            findPageByWebsiteId:findPageByWebsiteId
+            // "findPageById":"findPageById",
+            // "updatePage":"updatePage",
+            // "deletePage":"deletePage"
+            // "createPage":"createPage",
+
         };
         return api;
-        function createPage(websiteId,page){}
-        function findPageByWebsiteId(websiteId){}
-        function findPageById(pageId){}
-        function updatePage(pageId,page){}
-        function deletePage(pageId){}
+        // function createPage(websiteId,page){}
+        function findPageByWebsiteId(websiteId){
+            var resultSet=[];
+            for(var i in pages){
+                if(pages[i].websiteId===websiteId){
+                    resultSet.push(pages[i]);
+                }
+            }
+            return resultSet;
+        }
+        // function findPageById(pageId){}
+        // function updatePage(pageId,page){}
+        // function deletePage(pageId){}
     }
 })();
--- public/assignment3/views/page/page.controller.client.js
+++ public/assignment3/views/page/page.controller.client.js
@@ -4,24 +4,32 @@
 
 (function(){
     angular.module("WebAppMaker")
-        .controller("PageListController",PageListController)
-        .controller("NewPageController",NewPageController)
-        .controller("EditPageController",EditPageController);
-    function PageListController(){
+        .controller("PageListController",PageListController);
+        // .controller("NewPageController",NewPageController)
+        // .controller("EditPageController",EditPageController);
+    function PageListController($routeParams,PageService){
         var vm=this;
-
-    }
-    function NewPageController(){
-        var vm=this;
-
-    }
-    function EditPageController($routeParams,PageService){
-        var vm=this;
-        vm.pageId=$routeParams["pageId"];
         function init(){
-            vm.page=PageService.findPageById(vm.pageId);
+            var userId=$routeParams.uid;
+            var websiteId=$routeParams.wid;
+            var page=PageService.findPageByWebsiteId(websiteId);
+            vm.pages=page;
+            vm.userId=userId;
+            vm.websiteId=websiteId;
         }
         init();
-
     }
+
+    // function NewPageController(){
+    //     var vm=this;
+    // }
+//     function EditPageController($routeParams,PageService){
+//         var vm=this;
+//         vm.pageId=$routeParams["pageId"];
+//         function init(){
+//             vm.page=PageService.findPageById(vm.pageId);
+//         }
+//         init();
+//
+//     }
 })();
