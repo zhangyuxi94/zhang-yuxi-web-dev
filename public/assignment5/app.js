f07eb174b9418d53371b036d890fd925f8ec85b5
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-06-12 16:48:35 -0400

    Implement the HTML Widget

--- public/assignment5/app.js
+++ public/assignment5/app.js
@@ -2,5 +2,5 @@
  * Created by zhangyuxi on 2016/5/24.
  */
 (function () {
-    angular.module("WebAppMaker",["ngRoute","MyDirectives"]);
+    angular.module("WebAppMaker",["ngRoute","MyDirectives","textAngular"]);
 })();
